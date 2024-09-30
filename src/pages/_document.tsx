8e8bb8c9c43aa27b68f0d171f4242f85e37ca811
jaylen <jaylenjruiz@gmail.com>
2024-09-29 23:16:08 -0400

    okay hopefully fixed now?

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class ADocument extends Document {
     render() {
         return (
-            <Html lang="en" className="antialiased dark:bg-ThemeDark dark:text-white">
+            <Html lang="en" className="antialiased dark:bg-ThemeDark dark:text-white overflow-x-hidden min-h-screen w-full">
                 <Head>
                     <meta charSet="utf-8" />
                     <link
